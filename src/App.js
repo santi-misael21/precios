5d8f87f9b329bbdb16ac469624b8f26e25910c83
Santi Alamos <santiagoalamos2000@gmail.com>
2024-02-15 15:15:35 -0300

    15-2-23

--- src/App.js
+++ src/App.js
@@ -1,113 +1,174 @@
 import logo from './logo.svg';
 import './App.css';
+import { object } from './object';
 
 function App() {
+  let o = object;
+  let pot = o.Papa
+  let bat = o.Batata
+  let ceb = o.Cebolla
+  let zan = o.Zanahoria
+  let zap = o.Anco
+  let tom = o['Tomate perita']
+  let ban = o.Banana
+  let man = o.Manzana
+  let nar = o.Naranja
+  let fru = o.Frutilla
+  let lim = o.Limón
   return (
     <div className="App" style={{backgroundColor: 'black'}}>
     Lista de precios <br/>
  <br/>
     <br/> <div style={{display: 'block', width: '200px', margin: 'auto', color: 'yellow', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>
       Papa
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>bolsa </div><div>$12400</div> </div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/2 bolsa </div><div>$6600</div> </div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/4 bolsa </div><div>$3400</div> </div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div><div>$850</div> </div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div><div>$1650</div> </div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div><div>$825</div> </div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+      
+      <div>bolsa </div>
+      <div>{pot.bolsa}</div> </div>
+
+    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+      
+      <div>1/2 bolsa </div>
+      <div>{pot['1/2 bolsa']}</div> </div>
+
+    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+      
+      <div>1/4 bolsa </div>
+      <div>{pot['1/4 bolsa']}</div> </div>
+
+    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+      
+      <div>por kg </div>
+      <div>{pot['por kg']}</div> </div>
+
+    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+      
+      <div>por 2kg </div>
+      <div>{pot['por 2kg']}</div> </div>
+
+    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+      
+      <div>por kg oferta </div>
+      <div>{pot['por kg oferta']}</div> </div>
+
    <br/></div>
-   <br/>  <div style={{display: 'block', width: '200px', margin: 'auto', color: 'brown', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>
+   <br/>  <div style={{display: 'block', width: '200px', margin: 'auto', color: 'pink', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>
     Batata
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>bolsa </div><div>$7800</div> </div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/2 bolsa </div><div>$4200</div> </div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/4 bolsa </div><div>$2200</div> </div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div><div>$800</div> </div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div><div>$1500</div> </div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div><div>$750</div> </div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>bolsa </div><div>{bat.bolsa}</div> </div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/2 bolsa </div><div>{bat['1/2 bolsa']}</div> </div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/4 bolsa </div><div>{bat['1/4 bolsa']}</div> </div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div><div>{bat['por kg']}</div> </div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div><div>{bat['por 2kg']}</div> </div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div><div>{bat['por kg oferta']}</div> </div>
   <br/></div>
   <br/>  <div style={{display: 'block', width: '200px', margin: 'auto', color: 'wheat', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>
     Cebolla
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>bolsa </div><div>$14000</div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/2 bolsa </div><div>$7500</div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/4 bolsa </div><div>$3900</div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div><div>$950</div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div><div>$1850</div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div><div>$925</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>bolsa </div><div>{ceb.bolsa}</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/2 bolsa </div><div>{ceb['1/2 bolsa']}</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/4 bolsa </div><div>{ceb['1/4 bolsa']}</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div><div>{ceb['por kg']}</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div><div>{ceb['por 2kg']}</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div><div>{ceb['por kg oferta']}</div></div>
   <br/></div>
   <br/>  <div style={{display: 'block', width: '200px', margin: 'auto', color: 'orangered', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>
     Zanahoria
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>bolsa </div> <div>$7800</div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/2 bolsa </div> <div>$4200</div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/4 bolsa </div> <div>$2150</div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div> <div>$1000</div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div> <div>$1950</div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div> <div>$975</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>bolsa </div> <div>{zan.bolsa}</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/2 bolsa </div> <div>{zan['1/2 bolsa']}</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/4 bolsa </div> <div>{zan['1/4 bolsa']}</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div> <div>{zan['por kg']}</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div> <div>{zan['por 2kg']}</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div> <div>{zan['por kg oferta']}</div></div>
   <br/></div>
   <br/>  <div style={{display: 'block', width: '200px', margin: 'auto', color: 'orange', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>
     Anco
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>bolsa </div> <div>$7800</div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/2 bolsa </div> <div>$4200</div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/4 bolsa </div> <div>$2200</div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div> <div>$750</div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div> <div>$1400</div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div> <div>$700</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>bolsa </div> <div>{zap.bolsa}</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/2 bolsa </div> <div>{zap['1/2 bolsa']}</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/4 bolsa </div> <div>{zap['1/4 bolsa']}</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div> <div>{zap['por kg']}</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div> <div>{zap['por 2kg']}</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div> <div>{zap['por kg oferta']}</div></div>
   <br/></div>
   <br/>  
-  <div style={{display: 'block', width: '200px', margin: 'auto', color: 'red', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px', marginBottom: '20px'}}>
+  <div style={{display: 'block', width: '200px', margin: 'auto', color: 'red', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>
     Tomate perita
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>cajón </div> <div> $6200 </div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/2 cajón </div> <div> $3300 </div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/4 cajón </div> <div> $1700 </div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div> <div> $550 </div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div> <div> $1000 </div></div>
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div> <div> $500 </div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>cajón </div> <div> {tom.bolsa} </div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/2 cajón </div> <div> {tom['1/2 bolsa']} </div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>1/4 cajón </div> <div> {tom['1/4 bolsa']} </div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div> <div> {tom['por kg']} </div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div> <div> {tom['por 2kg']} </div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div> <div> {tom['por kg oferta']} </div></div>
   </div>
-  
+  <br/>  
   <div style={{display: 'block', width: '200px', margin: 'auto', color: 'green', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>
     Acelga
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>atado </div> <div>$800</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>atado </div> <div>{o.Acelga['por atado']}</div></div>
   </div>
   <br/>
   
   <div style={{display: 'block', width: '200px', margin: 'auto', color: 'yellowgreen', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>Perejil
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>atado </div> <div>$700</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>atado </div> <div>{o.Perejil['por atado']}</div></div>
   </div>
   <br/>
   
   <div style={{display: 'block', width: '200px', margin: 'auto', color: 'white', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>Verdeo
-    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>atado </div> <div>$1300</div></div>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}><div>atado </div> <div>{o.Verdeo['por atado']}</div></div>
   </div>
   <br/>
 <div style={{display: 'block', width: '200px', margin: 'auto', color: 'yellow', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>Banana
-  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div> <div>$2300</div></div>
-  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div> <div>$4500</div></div>
-  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div> <div>$2250</div></div>
+  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div> <div>{ban['por kg']}</div></div>
+  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div> <div>{ban['por 2kg']}</div></div>
+  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div> <div>{ban['por kg oferta']}</div></div>
   </div>
 <br/>
 <div style={{display: 'block', width: '200px', margin: 'auto', color: 'red', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>Frutilla
-  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div> <div>$2500</div></div>
-  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div> <div>$4900</div></div>
-  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div> <div>$2450</div></div>
+  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div> <div>{fru['por kg']}</div></div>
+  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div> <div>{fru['por 2kg']}</div></div>
+  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div> <div>{fru['por kg oferta']}</div></div>
   </div>
 <br/>
 <div style={{display: 'block', width: '200px', margin: 'auto', color: 'yellowgreen', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>Limón
-  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg $</div> <div>1300 </div></div>
-  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg $</div> <div>2300 </div></div>
-  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta $</div> <div>1150 </div></div>
+  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg $</div> <div>{lim['por kg']} </div></div>
+  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg $</div> <div>{lim['por 2kg']}</div></div>
+  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta $</div> <div>{lim['por kg oferta']} </div></div>
   </div>
 <br/>
 <div style={{display: 'block', width: '200px', margin: 'auto', color: 'red', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>Manzana
-  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div> <div>$2100</div></div>
-  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div> <div>$4100</div></div>
-  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div> <div>$2050</div></div>
+  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div> <div>{man['por kg']}</div></div>
+  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div> <div>{man['por 2kg']}</div></div>
+  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div> <div>{man['por kg oferta']}</div></div>
   </div>
 <br/>
 <div style={{display: 'block', width: '200px', margin: 'auto', color: 'orange', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>Naranja
-  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div> <div>$1300</div></div>
-  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div> <div>$2500</div></div>
-  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div> <div>$1250</div></div>
+  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg </div> <div>{nar['por kg']}</div></div>
+  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por 2kg </div> <div>{nar['por 2kg']}</div></div>
+  <div style={{display: 'flex', justifyContent: 'space-between'}}><div>por kg oferta </div> <div>{nar['por kg oferta']}</div></div>
   </div>
   <br/>
+  <div style={{display: 'block', width: '200px', margin: 'auto', color: 'wheat', backgroundColor: 'rgb(50,50,50,0.5)', padding: '10px'}}>
+    Ofertas de fruta
+    <br/><br/>
+    <div style={{color: 'orange'}}> 1kg manzana + 1kg frutilla = {man['por kg oferta']+fru['por kg oferta']} </div>
+    <br/><br/>
+    <div style={{color: 'yellowgreen'}}> 1kg frutilla + 1kg banana = {fru['por kg oferta']+ban['por kg oferta']} </div>
+    <br/><br/>
+    <div style={{color: 'orange'}}> 1kg frutilla + 1kg naranja = {nar['por kg oferta']+fru['por kg oferta']} </div>
+    <br/><br/>
+    <div style={{color: 'yellowgreen'}}> 1kg banana + 1kg naranja = {ban['por kg oferta']+nar['por kg oferta']} </div>
+    <br/><br/>
+    <div style={{color: 'orange'}}> 1kg banana + 1kg manzana = {man['por kg oferta']+ban['por kg oferta']} </div>
+    <br/><br/>
+    <div style={{color: 'yellowgreen'}}> 1kg naranja + 1kg banana + 1kg manzana = {nar['por kg oferta']+ban['por kg oferta']+man['por kg oferta']} </div>
+    <br/><br/>
+    <div style={{color: 'orange'}}> 1kg naranja +  1kg frutilla + 1kg manzana = {nar['por kg oferta']+fru['por kg oferta']+man['por kg oferta']} </div>
+    <br/><br/>
+    <div style={{color: 'yellowgreen'}}> 1kg naranja +  1kg frutilla + 1kg banana = {nar['por kg oferta']+fru['por kg oferta']+ban['por kg oferta']} </div>
+    <br/><br/>
+    <div style={{color: 'orange'}}> 1kg frutilla + 1kg banana + 1kg manzana = {ban['por kg oferta']+fru['por kg oferta']+man['por kg oferta']} </div>
+    <br/><br/>
+    <div style={{color: 'yellowgreen'}}> 1kg frutilla + 1kg naranja + 1kg banana + 1kg manzana = {man['por kg oferta']+fru['por kg oferta']+ban['por kg oferta']+nar['por kg oferta']} </div>
     </div>
+    <br/><br/>
+  </div>
   );
 }<br/>
 
---
+++ src/object.js
@@ -0,0 +1,96 @@
+export let object = {
+    Papa: {
+      bolsa: '(17kg) $11300',
+      '1/2 bolsa': '(8.5kg) $6000',
+      '1/4 bolsa': '(4.25kg) $3100',
+      'por kg': '$750',
+      'por 2kg': '$1450',
+      'por kg oferta': 725
+    },
+    Batata: {
+      bolsa: '(12kg) $7500',
+      '1/2 bolsa': '(6kg) $4000',
+      '1/4 bolsa': '(3kg) $2100',
+      'por kg': '$750',
+      'por 2kg': '$1400',
+      'por kg oferta': 700
+    },
+    Cebolla: {
+      bolsa: '(17kg) $13500',
+      '1/2 bolsa': '(8.5kg) $7200',
+      '1/4 bolsa': '(4.25kg) $3800',
+      'por kg': '$900',
+      'por 2kg': '$1750',
+      'por kg oferta': 875
+    },
+    Zanahoria: {
+      bolsa: '(9kg) $7500',
+      '1/2 bolsa': '(4.5kg) $4000',
+      '1/4 bolsa': '(2.25kg) $2100',
+      'por kg': '$950',
+      'por 2kg': '$1800',
+      'por kg oferta': 900
+    },
+    Anco: {
+      bolsa: '(13kg) $7500',
+      '1/2 bolsa': '(6.5kg) $4000',
+      '1/4 bolsa': '(3.25kg) $2100',
+      'por kg': '$700',
+      'por 2kg': '$1300',
+      'por kg oferta': 650
+    },
+    'Tomate perita': {
+      'por kg': '$450',
+      'por 2kg': '$800',
+      'por kg oferta': 400
+    },
+    Acelga: {
+      'por atado': '$850',
+    },
+    Perejil: {
+      'por atado': '$800',
+    },
+    Verdeo: {
+      'por atado': '$1300',
+    },
+    Banana: {
+        cajón: '(19kg) $37500',
+        '1/2 cajón': '(9.5kg) $20000',
+        '1/4 cajón': '(4.75kg) $10400',
+        'por kg': '$2200',
+        'por 2kg': '$4300',
+        'por kg oferta': 2150
+    },
+    Manzana: {
+        cajón: '(9kg) $16500',
+        '1/2 cajón': '(4.5kg) $8800',
+        '1/4 cajón': '(2.25kg) $4600',
+        'por kg': '$2100',
+        'por 2kg': '$4100',
+        'por kg oferta': 2050
+    },
+    Naranja: {
+        cajón: '(17kg) $15000',
+        '1/2 cajón': '(8.5kg) $8000',
+        '1/4 cajón': '(4.25kg) $4200',
+        'por kg': '$1000',
+        'por 2kg': '$1900',
+        'por kg oferta': 950
+    },
+    Frutilla: {
+        cajón: '(4.5kg) $9000',
+        '1/2 cajón': '(2.25kg) $4800',
+        '1/4 cajón': '(1.125kg) $2500',
+        'por kg': '$2200',
+        'por 2kg': '$4300',
+        'por kg oferta': 2150
+    },
+    'Limón': {
+        cajón: '(7kg) $7500',
+        '1/2 cajón': '(3.5kg) $4000',
+        '1/4 cajón': '(1.75kg) $2100',
+        'por kg': '$1250',
+        'por 2kg': '$2400',
+        'por kg oferta': 1200
+    }
+}
