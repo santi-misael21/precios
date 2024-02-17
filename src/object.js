eba77191cd5569226b66ac43e464a2d176a88425
Santi Alamos <santiagoalamos2000@gmail.com>
2024-02-17 17:59:43 -0300

    17-2-24

--- src/object.js
+++ src/object.js
@@ -1,58 +1,59 @@
 export let object = {
-    Papa: {
-      bolsa: '(17kg) $11300',
-      '1/2 bolsa': '(8.5kg) $6000',
-      '1/4 bolsa': '(4.25kg) $3100',
-      'por kg': '$750',
-      'por 2kg': '$1450',
-      'por kg oferta': 725
-    },
-    Batata: {
-      bolsa: '(12kg) $7500',
-      '1/2 bolsa': '(6kg) $4000',
-      '1/4 bolsa': '(3kg) $2100',
-      'por kg': '$750',
-      'por 2kg': '$1400',
-      'por kg oferta': 700
-    },
-    Cebolla: {
-      bolsa: '(17kg) $13500',
-      '1/2 bolsa': '(8.5kg) $7200',
-      '1/4 bolsa': '(4.25kg) $3800',
-      'por kg': '$900',
-      'por 2kg': '$1750',
-      'por kg oferta': 875
-    },
-    Zanahoria: {
-      bolsa: '(9kg) $7500',
-      '1/2 bolsa': '(4.5kg) $4000',
-      '1/4 bolsa': '(2.25kg) $2100',
-      'por kg': '$950',
-      'por 2kg': '$1800',
-      'por kg oferta': 900
-    },
-    Anco: {
-      bolsa: '(13kg) $7500',
-      '1/2 bolsa': '(6.5kg) $4000',
-      '1/4 bolsa': '(3.25kg) $2100',
-      'por kg': '$700',
-      'por 2kg': '$1300',
-      'por kg oferta': 650
-    },
-    'Tomate perita': {
-      'por kg': '$450',
-      'por 2kg': '$800',
-      'por kg oferta': 400
-    },
-    Acelga: {
-      'por atado': '$2000',
-    },
-    Perejil: {
-      'por atado': '$1500',
-    },
-    Verdeo: {
-      'por atado': '$2000',//'$540500 (quinientos cuarenta mil quinientos pesos)',
-    },
+  Papa: {
+    bolsa: '(17kg) $11300',
+    '1/2 bolsa': '(8.5kg) $6000',
+    '1/4 bolsa': '(4.25kg) $3100',
+    'por kg': '$750',
+    'por 2kg': '$1450',
+    'por kg oferta': 725
+  },
+  Batata: {
+    bolsa: '(12kg) $6000',
+    '1/2 bolsa': '(6kg) $3200',
+    '1/4 bolsa': '(3kg) $1700',
+    'por kg': '$600',
+    'por 2kg': '$1150',
+    'por kg oferta': 575
+  },
+  Cebolla: {
+    bolsa: '(17kg) $11300',
+    '1/2 bolsa': '(8.5kg) $6000',
+    '1/4 bolsa': '(4.25kg) $3100',
+    'por kg': '$750',
+    'por 2kg': '$1450',
+    'por kg oferta': 725
+  },
+  Zanahoria: {
+    bolsa: '(9kg) $5700',
+    '1/2 bolsa': '(4.5kg) $3000',
+    '1/4 bolsa': '(2.25kg) $1600',
+    'por kg': '$750',
+    'por 2kg': '$1450',
+    'por kg oferta': 725
+  },
+  Anco: {
+    bolsa: '(13kg) $6800',
+    '1/2 bolsa': '(6.5kg) $3600',
+    '1/4 bolsa': '(3.25kg) $1900',
+    'por kg': '$650',
+    'por 2kg': '$1200',
+    'por kg oferta': 600
+  },
+  'Tomate perita': {
+    'por kg': '$450',
+    'por 2kg': '$800',
+    'por kg oferta': 400
+  },
+  Acelga: {
+    'por atado': '$2000',
+  },
+  Perejil: {
+    'por atado': '$1500',
+  },
+  Verdeo: {
+    'por atado': '$2000',
+    // '$540500 (quinientos cuarenta mil quinientos pesos)',
+  },
     Banana: {
         cajón: '(19kg) $37500',
         '1/2 cajón': '(9.5kg) $20000',
