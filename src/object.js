455a9c63eddcf0d7e20e91a889252d04c090bb2a
Santi Alamos <santiagoalamos2000@gmail.com>
2024-03-06 16:01:04 -0300

    Out Stock

--- src/object.js
+++ src/object.js
@@ -9,9 +9,9 @@ export let object = {
     color: 'yellow',
   },
   Cebolla: {
-    'Bolsa (17kg)': '$9200',
-    'llevando 1 bolsa': '$540 por kg',
-    '1/2 bolsa (8.5kg)': '$4900',
+    // 'Bolsa (17kg)': '$9200',
+    // 'llevando 1 bolsa': '$540 por kg',
+    // '1/2 bolsa (8.5kg)': '$4900',
     '1/4 bolsa (4.25kg)': '$2500',
     'llevando 1/4 bolsa': '$590 por kg',
     'Por kg': '$650',
@@ -20,15 +20,15 @@ export let object = {
     'llevando 3kg': '$600 por kg',
     color: 'wheat',
   },
-  Anco: {
-    // bolsa: '(13kg) $6800',
-    // '1/2 bolsa': '(6.5kg) $3600',
-    // '1/4 bolsa': '(3.25kg) $1900',
-    'Por kg': '$650',
-    'Por 2kg': 1200,
-    'llevando 2kg': '$600 por kg ',
-    color: 'orange',
-  },
+//   Anco: {
+//     // bolsa: '(13kg) $6800',
+//     // '1/2 bolsa': '(6.5kg) $3600',
+//     // '1/4 bolsa': '(3.25kg) $1900',
+//     'Por kg': '$650',
+//     'Por 2kg': 1200,
+//     'llevando 2kg': '$600 por kg ',
+//     color: 'orange',
+//   },
   'Lechuga criolla': {
 	'1/2 cajón': '$4500',
 	'llevando por kg': 'Consultar',
@@ -112,16 +112,16 @@ export let object = {
   //   'Por kg': '$900',
   //   color: 'yellowgreen'
   // },
-  Banana: {
-    // bolsa: '(19kg) $sin precio',
-    // '1/2 bolsa': '(9.5kg) $sin precio',
-    // '1/4 cajón (4.75kg)': '$6000',
-    'Por kg': '$1600',
-    'Por 2kg': 3000,
-    'Por 3kg': '$3900',
-    'llevando 3kg': '$1300 por kg',
-    color: 'yellow',
-  },
+//   Banana: {
+//     // bolsa: '(19kg) $sin precio',
+//     // '1/2 bolsa': '(9.5kg) $sin precio',
+//     // '1/4 cajón (4.75kg)': '$6000',
+//     'Por kg': '$1600',
+//     'Por 2kg': 3000,
+//     'Por 3kg': '$3900',
+//     'llevando 3kg': '$1300 por kg',
+//     color: 'yellow',
+//   },
   // Manzana: {
   //   // bolsa: '(17kg) $sin precio',
   //   // '1/2 bolsa': '(8.5kg) $sin precio',
@@ -149,16 +149,16 @@ export let object = {
   //   'por 2kg': 4800,
   //   'por kg oferta': 2400
   // },
-  Durazno: {
-    // bolsa: '(12kg) $sin precio',
-    // '1/2 bolsa': '(6kg) $sin precio',
-    // '1/4 cajón (3kg)': '$3600',
-    'Por kg': '$1300',
-    'Por 2kg': 2400,
-    'Por 3kg': '$3300',
-    'llevando 3kg': '$1100 por kg',
-    color: 'wheat',
-  },
+//   Durazno: {
+//     // bolsa: '(12kg) $sin precio',
+//     // '1/2 bolsa': '(6kg) $sin precio',
+//     // '1/4 cajón (3kg)': '$3600',
+//     'Por kg': '$1300',
+//     'Por 2kg': 2400,
+//     'Por 3kg': '$3300',
+//     'llevando 3kg': '$1100 por kg',
+//     color: 'wheat',
+//   },
   'Limón': {
     // bolsa: '(16kg) $7750',
     // '1/2 bolsa': '(8kg) $2800',
@@ -168,42 +168,42 @@ export let object = {
     'llevando 2kg': '$750 por kg',
     color: 'greenyellow',
   },
-  'Ofertas de fruta':{
-    color: 'yellowgreen',
-    'Martes 5/3/24': '+10% descuento en frutas'
-  },
-  'Oferta 1':{
-    "<b>1kg naranja <br/> + 1kg banana</b><br/>": '',
-    color: 'orangered'
-  },
-  'Oferta 2':{
-    '<b>1kg naranja <br/> + 1kg durazno</b><br/>': '',
-    color: 'orange'
-  },
-  'Oferta 3':{
-    '<b>1kg durazno <br/> + 1kg banana</b><br/>': '',
-    color: 'yellow'
-  },
-  'Oferta 4':{
-    '<b>1kg durazno <br/> + 1kg naranja <br/> + 1kg banana</b><br/>': '',
-    color: 'wheat'
-  },
+//   'Ofertas de fruta':{
+//     color: 'yellowgreen',
+//     'Miércoles 6/3/24': '+10% descuento en frutas'
+//   },
+//   'Oferta 1':{
+//     "<b>1kg naranja <br/> + 1kg banana</b><br/>": '',
+//     color: 'orangered'
+//   },
+//   'Oferta 2':{
+//     '<b>1kg naranja <br/> + 1kg durazno</b><br/>': '',
+//     color: 'orange'
+//   },
+//   'Oferta 3':{
+//     '<b>1kg durazno <br/> + 1kg banana</b><br/>': '',
+//     color: 'yellow'
+//   },
+//   'Oferta 4':{
+//     '<b>1kg durazno <br/> + 1kg naranja <br/> + 1kg banana</b><br/>': '',
+//     color: 'wheat'
+//   },
 }
-let o = object;
+// let o = object;
 // let o1 = o["Oferta 1"]
 // let o2 = o["Oferta 2"]
 // let o3 = o["Oferta 3"]
 // let o4 = o["Oferta 4"]
 
-let nar = o.Naranja["Por 2kg"];
-let ban = o.Banana["Por 2kg"];
-let dur = o.Durazno["Por 2kg"];
+// let nar = o.Naranja["Por 2kg"];
+// let ban = o.Banana["Por 2kg"];
+// let dur = o.Durazno["Por 2kg"];
 
-object["Oferta 1"]["<b>1kg naranja <br/> + 1kg banana</b><br/>"]=
-`$${nar/2+ban/2}`
-object["Oferta 2"]["<b>1kg naranja <br/> + 1kg durazno</b><br/>"]=
-`$${nar/2+dur/2}`
-object["Oferta 3"]["<b>1kg durazno <br/> + 1kg banana</b><br/>"]=
-`$${dur/2+ban/2}`
-object["Oferta 4"]["<b>1kg durazno <br/> + 1kg naranja <br/> + 1kg banana</b><br/>"]=
-`$${dur/2+nar/2+ban/2}`
+// object["Oferta 1"]["<b>1kg naranja <br/> + 1kg banana</b><br/>"]=
+// `$${nar/2+ban/2}`
+// object["Oferta 2"]["<b>1kg naranja <br/> + 1kg durazno</b><br/>"]=
+// `$${nar/2+dur/2}`
+// object["Oferta 3"]["<b>1kg durazno <br/> + 1kg banana</b><br/>"]=
+// `$${dur/2+ban/2}`
+// object["Oferta 4"]["<b>1kg durazno <br/> + 1kg naranja <br/> + 1kg banana</b><br/>"]=
+// `$${dur/2+nar/2+ban/2}`
